08eb19728aa28b253a8329a4216210d7efcdf04a
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-09-27 20:16:55 +0200

    here we go

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,7 @@
--- Does exact same thing as setup_mysql. For some reason acts as a test too. SO WE DONT NEED ONE!!!
+-- prepares a MySQL server for the project
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
