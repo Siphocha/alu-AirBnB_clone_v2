e820600466147ffa610ca5b6e0bdd1ba916ec281
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-10-10 18:05:15 +0200

    ahhh

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""
-start Flask application
-"""
+"""testing and running flask server"""
 
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+def Hello_HBNB():
+    """prints out when ran"""
+    return "Hello HBNB!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    """Define which port application runs from"""
+    app.run(host="0.0.0.0", port="5000")
+
+
