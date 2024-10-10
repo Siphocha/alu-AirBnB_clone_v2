f9eb34363f29655b84e405c6ebce1ac462e2472e
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-10-10 18:42:19 +0200

    HATEEE THIS TEDIOUS WORK

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,16 +4,19 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def Hello_HBNB():
     """prints out when ran"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def HBNB():
     """Checking to see normal returning"""
     return "HBNB"
 
+
 if __name__ == '__main__':
     """Define which port application runs from"""
-    app.run(host="0.0.0.0", port=5000) 
+    app.run(host="0.0.0.0", port="5000")
